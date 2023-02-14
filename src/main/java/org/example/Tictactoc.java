872edae91b8468e3cffc539e1b8ae3afcf425296
NITHISH KUMAR M <123449276+NITHISH0403@users.noreply.github.com>
2023-02-14 11:42:52 +0530

    Update Tictactoc.java

--- src/main/java/org/example/Tictactoc.java
+++ src/main/java/org/example/Tictactoc.java
@@ -3,11 +3,14 @@ package org.example;
 import java.util.Scanner;
 import java.util.logging.*;
 class Game{
-        static String[][] a = new String[3][3];
+        static final String[][] a = new String[3][3];
         int search;
         Scanner s = new Scanner(System.in);
         Logger l = Logger.getLogger("NITHISH");
         int player = 0;
+        String player1 = "Player 1 is Winner.";
+        String player2 = "Player 1 is Winner.";
+        String draw = "The Match is Draw.";
     void players(){
         while (true) {
             //Player 1
@@ -40,21 +43,21 @@ class Game{
                     for (int i = 0; i < 3; i++) {
                         //hor
                         if (a[i][0].equals(a[i][1]) && a[i][1].equals(a[i][2]) && a[i][2].equals(a[i][0])) {
-                            l.info("Player 1 is Winner.");
+                            l.info(player1);
                             System.exit(0);
                         }
                         //ver
                         else if (a[0][i].equals(a[1][i]) && a[1][i].equals(a[2][i]) && a[2][i].equals(a[0][i])) {
-                            l.info("Player 1 is Winner.");
+                            l.info(player1);
                             System.exit(0);
                         }
                         //left to right cross
                         else if (a[0][0].equals(a[1][1]) && a[1][1].equals(a[2][2])) {
-                            l.info("Player 1 is Winner.");
+                            l.info(player1);
                             System.exit(0);
                             break;
                         } else if (a[0][2].equals(a[1][1]) && a[1][1].equals(a[2][0])) {
-                            l.info("Player 1 is Winners.");
+                            l.info(player1);
                             System.exit(0);
                             break;
                         }
@@ -89,23 +92,23 @@ class Game{
                 }
             }
             else{
-                l.info("The Match is Draw.");
+                l.info(draw);
                 System.exit(0);
             }
             for(int i = 0; i<3; i++){
                 //hor
                 if(a[i][0].equals(a[i][1]) && a[i][1].equals(a[i][2]) && a[i][2].equals(a[i][0])){
-                    l.info("Player 2 is Winner.");
+                    l.info(player2);
                     System.exit(0);
                 }
                 //ver
                 else if(a[0][i].equals(a[1][i]) && a[1][i].equals(a[2][i]) && a[2][i].equals(a[0][i])){
-                    l.info("Player 2 is Winner.");
+                    l.info(player2);
                     System.exit(0);
                 }
                 //left to right cross
                 else if (a[0][0].equals(a[1][1]) && a[1][1].equals(a[2][2])) {
-                    l.info("Player 2 is Winner.");
+                    l.info(player2);
                     System.exit(0);
                     break;
                 }
