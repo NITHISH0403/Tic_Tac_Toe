fba4812e0b939d30e596f14fff1a7dfc08b57507
NITHISH KUMAR M <123449276+NITHISH0403@users.noreply.github.com>
2023-02-14 17:52:43 +0530

    Update Tictactoc.java

--- src/main/java/org/example/Tictactoc.java
+++ src/main/java/org/example/Tictactoc.java
@@ -3,12 +3,11 @@ package org.example;
 import java.util.Scanner;
 class Game{
     int search;
-    Scanner s = new Scanner(System.in);
+    static Scanner s = new Scanner(System.in);
     int play = 0;
     String player1 = "Player 1 is Winner.";
     String player2 = "Player 2 is Winner.";
     String draw = "The Match is Draw.";
-    static String design = "+-------+-------+-------+";
     void players(String[][] a){
         while (true) {
             //Player 1
@@ -58,39 +57,38 @@ class Game{
         }
     }
     void display(String[][] a){
-        System.out.println(design);
         for (int i = 0; i < 3; i++) {
             System.out.print("|");
             for (int j = 0; j < 3; j++) {
                 String n = String.valueOf(a[i][j]);
                 System.out.print("\t" + n + "\t" + "|");
             }
-            System.out.println("\n" + design);
+            System.out.println("\n");
         }
     }
-    void win(String[][] a,String player2)
+    void win(String[][] a,String x)
     {
         for(int i = 0; i<3; i++){
             //hor
             if(a[i][0].equals(a[i][1]) && a[i][1].equals(a[i][2]) && a[i][2].equals(a[i][0])){
-                System.out.println(player2);
+                System.out.println(x);
                 System.exit(0);
 
             }
             //ver
             else if(a[0][i].equals(a[1][i]) && a[1][i].equals(a[2][i]) && a[2][i].equals(a[0][i])){
-                System.out.println(player2);
+                System.out.println(x);
                 System.exit(0);
             }
             //left to right cross
             else if (a[0][0].equals(a[1][1]) && a[1][1].equals(a[2][2])) {
-                System.out.println(player2);
+                System.out.println(x);
                 System.exit(0);
                 break;
             }
             //right to left cross
             else if (a[0][2].equals(a[1][1]) && a[1][1].equals(a[2][0])) {
-                System.out.println(player2);
+                System.out.println(x);
                 System.exit(0);
                 break;
             }
@@ -99,7 +97,7 @@ class Game{
 }
 public class Tictactoc extends Game{
     public static void main(String[] args) {
-        String[][] a = new String[3][3];
+        String[][] a = new String[10][10];
         int value = 1;
         Tictactoc t = new Tictactoc();
         for (int i = 0; i < 3; i++) {
@@ -107,15 +105,7 @@ public class Tictactoc extends Game{
                 a[i][j] = String.valueOf(value++);
             }
         }
-        System.out.println(design);
-        for (int i = 0; i < 3; i++) {
-            System.out.print("|");
-            for (int j = 0; j < 3; j++) {
-                String n = String.valueOf(a[i][j]);
-                System.out.print("\t" + n + "\t" + "|");
-            }
-            System.out.println("\n" + design);
-        }
+        t.display(a);
         t.players(a);
     }
 }
