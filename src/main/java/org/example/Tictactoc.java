166f267fc56199c863ae5b59d0c281b869da23b2
NITHISH KUMAR M <123449276+NITHISH0403@users.noreply.github.com>
2023-02-14 11:14:51 +0530

    Update Tictactoc.java

--- src/main/java/org/example/Tictactoc.java
+++ src/main/java/org/example/Tictactoc.java
@@ -1,17 +1,19 @@
 package org.example;
 
 import java.util.Scanner;
+import java.util.logging.*;
 class Game{
         public static String[][] a = new String[3][3];
         int search;
         Scanner s = new Scanner(System.in);
+        Logger l = Logger.getLogger("NITHISH");
         int player = 0;
     void player(){
         while (true) {
             //Player 1
             if(player != 9) {
                 if (player % 2 == 0) {
-                    System.out.println("Choose the place in Player 1 -> X:");
+                    l.info("Choose the place in Player 1 -> X:");
                     search = s.nextInt();
                     for (int i = 0; i < 3; i++) {
                         for (int j = 0; j < 3; j++) {
@@ -21,37 +23,38 @@ class Game{
                                     player++;
                                 }
                                 else {
-                                    System.out.println("Please, Choose another place.");
+                                    l.info("Please, Choose another place.");
                                 }
                             }
                         }
                     }
                     System.out.println("+-------+-------+-------+");
                     for (int i = 0; i < 3; i++) {
-                        System.out.print("|");
+                        l.info("|");
                         for (int j = 0; j < 3; j++) {
-                            System.out.print("\t" + a[i][j] + "\t" + "|");
+                            String n = String.valueOf(a[i][j]);
+                            l.log(Level.INFO,()->"\t" + n + "\t" + "|");
                         }
-                        System.out.println("\n+-------+-------+-------+");
+                        l.info("\n+-------+-------+-------+");
                     }
                     for (int i = 0; i < 3; i++) {
                         //hor
                         if (a[i][0].equals(a[i][1]) && a[i][1].equals(a[i][2]) && a[i][2].equals(a[i][0])) {
-                            System.out.println("Player 1 is Winner.");
+                            l.info("Player 1 is Winner.");
                             System.exit(0);
                         }
                         //ver
                         else if (a[0][i].equals(a[1][i]) && a[1][i].equals(a[2][i]) && a[2][i].equals(a[0][i])) {
-                            System.out.println("Player 1 is Winner.");
+                            l.info("Player 1 is Winner.");
                             System.exit(0);
                         }
                         //left to right cross
                         else if (a[0][0].equals(a[1][1]) && a[1][1].equals(a[2][2])) {
-                            System.out.println("Player 1 is Winner.");
+                            l.info("Player 1 is Winner.");
                             System.exit(0);
                             break;
                         } else if (a[0][2].equals(a[1][1]) && a[1][1].equals(a[2][0])) {
-                            System.out.println("Player 1 is Winners.");
+                            l.info("Player 1 is Winners.");
                             System.exit(0);
                             break;
                         }
@@ -59,7 +62,7 @@ class Game{
                 }
                 //player 2
                 else {
-                    System.out.println("Choose the place in Player 2 -> O:");
+                    l.info("Choose the place in Player 2 -> O:");
                     search = s.nextInt();
                     for (int i = 0; i < 3; i++) {
                         for (int j = 0; j < 3; j++) {
@@ -69,39 +72,40 @@ class Game{
                                     player++;
                                 }
                                 else {
-                                    System.out.println("Please, Choose another place.");
+                                    l.info("Please, Choose another place.");
                                 }
                             }
                         }
                     }
-                    System.out.println("+-------+-------+-------+");
+                    l.info("+-------+-------+-------+");
                     for (int i = 0; i < 3; i++) {
                         System.out.print("|");
                         for (int j = 0; j < 3; j++) {
-                            System.out.print("\t" + a[i][j] + "\t" + "|");
+                            String n = String.valueOf(a[i][j]);
+                            l.log(Level.INFO,()->"\t" + n + "\t" + "|");
                         }
-                        System.out.println("\n+-------+-------+-------+");
+                        l.info("\n+-------+-------+-------+");
                     }
                 }
             }
             else{
-                System.out.println("The Match is Draw.");
+                l.info("The Match is Draw.");
                 System.exit(0);
             }
             for(int i = 0; i<3; i++){
                 //hor
                 if(a[i][0].equals(a[i][1]) && a[i][1].equals(a[i][2]) && a[i][2].equals(a[i][0])){
-                    System.out.println("Player 2 is Winner.");
+                    l.info("Player 2 is Winner.");
                     System.exit(0);
                 }
                 //ver
                 else if(a[0][i].equals(a[1][i]) && a[1][i].equals(a[2][i]) && a[2][i].equals(a[0][i])){
-                    System.out.println("Player 2 is Winner.");
+                    l.info("Player 2 is Winner.");
                     System.exit(0);
                 }
                 //left to right cross
                 else if (a[0][0].equals(a[1][1]) && a[1][1].equals(a[2][2])) {
-                    System.out.println("Player 2 is Winner.");
+                    l.info("Player 2 is Winner.");
                     System.exit(0);
                     break;
                 }
