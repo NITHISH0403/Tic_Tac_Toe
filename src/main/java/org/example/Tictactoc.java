c7a3ec9a18a3e13d01616f241d1bce9ba4239bd2
NITHISH KUMAR M <123449276+NITHISH0403@users.noreply.github.com>
2023-02-14 13:05:27 +0530

    Update Tictactoc.java

--- src/main/java/org/example/Tictactoc.java
+++ src/main/java/org/example/Tictactoc.java
@@ -1,10 +1,12 @@
 package org.example;
 
 import java.util.Scanner;
+import java.util.logging.*;
 class Game{
         static String[][] a = new String[3][3];
         int search;
         Scanner s = new Scanner(System.in);
+        Logger l = Logger.getlogger("NITHISH");
         int player = 0;
         String player1 = "Player 1 is Winner.";
         String player2 = "Player 2 is Winner.";
@@ -15,7 +17,7 @@ class Game{
             //Player 1
             if(player != 9) {
                 if (player % 2 == 0) {
-                    System.out.println("Choose the place in Player 1 -> X:");
+                    l.info("Choose the place in Player 1 -> X:");
                     search = s.nextInt();
                     for (int i = 0; i < 3; i++) {
                         for (int j = 0; j < 3; j++) {
@@ -25,40 +27,40 @@ class Game{
                                     player++;
                                 }
                                 else {
-                                    System.out.println("Please, Choose another place.");
+                                    l.info("Please, Choose another place.");
                                 }
                             }
                         }
                     }
-                    System.out.println(design);
+                    l.log(Level.INFO,()->design);
                     for (int i = 0; i < 3; i++) {
-                        System.out.print("|");
+                        l.info("|");
                         for (int j = 0; j < 3; j++) {
                             String n = String.valueOf(a[i][j]);
-                            System.out.print("\t" + n + "\t" + "|");
+                            l.log(Level.INFO,()->"\t" + n + "\t" + "|");
                         }
-                        System.out.println("\n" + design);
+                        l.log(Level.INFO,()->"\n" + design);
                     }
                     for (int i = 0; i < 3; i++) {
                         //hor
                         if (a[i][0].equals(a[i][1]) && a[i][1].equals(a[i][2]) && a[i][2].equals(a[i][0])) {
-                            System.out.println(player1);
+                            l.log(Level.INFO,()->player1);
                             System.exit(0);
                         }
                         //ver
                         else if (a[0][i].equals(a[1][i]) && a[1][i].equals(a[2][i]) && a[2][i].equals(a[0][i])) {
-                            System.out.println(player1);
+                            l.log(Level.INFO,()->player1);
                             System.exit(0);
                         }
                         //left to right cross
                         else if (a[0][0].equals(a[1][1]) && a[1][1].equals(a[2][2])) {
-                            System.out.println(player1);
+                            l.log(Level.INFO,()->player1);
                             System.exit(0);
                             break;
                         }
                         //right to left cross
                         else if (a[0][2].equals(a[1][1]) && a[1][1].equals(a[2][0])) {
-                            System.out.println(player1);
+                            l.log(Level.INFO,()->player1);
                             System.exit(0);
                             break;
                         }
@@ -66,7 +68,7 @@ class Game{
                 }
                 //player 2
                 else {
-                    System.out.println("Choose the place in Player 2 -> O:");
+                    l.info("Choose the place in Player 2 -> O:");
                     search = s.nextInt();
                     for (int i = 0; i < 3; i++) {
                         for (int j = 0; j < 3; j++) {
@@ -76,46 +78,46 @@ class Game{
                                     player++;
                                 }
                                 else {
-                                    System.out.println("Please, Choose another place.");
+                                    l.info("Please, Choose another place.");
                                 }
                             }
                         }
                     }
-                    System.out.println(design);
+                    l.log(Level.INFO,()->design);
                     for (int i = 0; i < 3; i++) {
-                        System.out.print("|");
+                        l.info("|");
                         for (int j = 0; j < 3; j++) {
                             String n = String.valueOf(a[i][j]);
-                            System.out.print("\t" + n + "\t" + "|");
+                            l.log(Level.INFO,()->"\t" + n + "\t" + "|");
                         }
-                        System.out.println("\n" + design);
+                        l.log(Level.INFO,()->"\n" + design);
                     }
                 }
             }
             else{
-                System.out.println(draw);
+               l.log(Level.INFO,()->draw);
                 System.exit(0);
             }
             for(int i = 0; i<3; i++){
                 //hor
                 if(a[i][0].equals(a[i][1]) && a[i][1].equals(a[i][2]) && a[i][2].equals(a[i][0])){
-                    System.out.println(player2);
+                    l.log(Level.INFO,()->player2);
                     System.exit(0);
                 }
                 //ver
                 else if(a[0][i].equals(a[1][i]) && a[1][i].equals(a[2][i]) && a[2][i].equals(a[0][i])){
-                    System.out.println(player2);
+                    l.log(Level.INFO,()->player2);
                     System.exit(0);
                 }
                 //left to right cross
                 else if (a[0][0].equals(a[1][1]) && a[1][1].equals(a[2][2])) {
-                    System.out.println(player2);
+                    l.log(Level.INFO,()->player2);
                     System.exit(0);
                     break;
                 }
                 //right to left cross
                 else if (a[0][2].equals(a[1][1]) && a[1][1].equals(a[2][0])) {
-                    System.out.println(player2);
+                    l.log(Level.INFO,()->player2);
                     System.exit(0);
                     break;
                 }
@@ -132,14 +134,14 @@ public class Tictactoc extends Game{
                 a[i][j] = String.valueOf(value++);
             }
         }
-        System.out.println(design);
+        l.log(Level.INFO,()->design);
         for (int i = 0; i < 3; i++) {
-            System.out.print("|");
+            l.info("|");
             for (int j = 0; j < 3; j++) {
                 String n = String.valueOf(a[i][j]);
-                System.out.print("\t" + n + "\t" + "|");
+                l.log(Level.INFO,()->"\t" + n + "\t" + "|");
             }
-            System.out.println("\n" + design);
+            l.log(Level.INFO,()->"\n" + design);
         }
         t.players();
     }
