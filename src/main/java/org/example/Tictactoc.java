614b4a6e881e7c19f726ea8e2d62163ae3d6f4e6
NITHISH KUMAR M <123449276+NITHISH0403@users.noreply.github.com>
2023-02-14 14:52:31 +0530

    Update Tictactoc.java

--- src/main/java/org/example/Tictactoc.java
+++ src/main/java/org/example/Tictactoc.java
@@ -16,112 +16,85 @@ class Game{
                 if (player % 2 == 0) {
                     System.out.println("Choose the place in Player 1 -> X:");
                     search = s.nextInt();
-                    for (int i = 0; i < 3; i++) {
-                        for (int j = 0; j < 3; j++) {
-                            if ((a[i][j].equals(String.valueOf(search)))) {
-                                if (!a[i][j].equals("X") || !a[i][j].equals("O")) {
-                                    a[i][j] = "X";
-                                    player++;
-                                }
-                                else {
-                                    System.out.println("Please, Choose another place.");
-                                }
-                            }
-                        }
-                    }
-                    System.out.println(design);
-                    for (int i = 0; i < 3; i++) {
-                        System.out.print("|");
-                        for (int j = 0; j < 3; j++) {
-                            String n = String.valueOf(a[i][j]);
-                            System.out.print("\t" + n + "\t" + "|");
-                        }
-                        System.out.println("\n" + design);
-                    }
-                    for (int i = 0; i < 3; i++) {
-                        //hor
-                        if (a[i][0].equals(a[i][1]) && a[i][1].equals(a[i][2]) && a[i][2].equals(a[i][0])) {
-                            System.out.println(player1);
-                            System.exit(0);
-                        }
-                        //ver
-                        else if (a[0][i].equals(a[1][i]) && a[1][i].equals(a[2][i]) && a[2][i].equals(a[0][i])) {
-                            System.out.println(player1);
-                            System.exit(0);
-                        }
-                        //left to right cross
-                        else if (a[0][0].equals(a[1][1]) && a[1][1].equals(a[2][2])) {
-                            System.out.println(player1);
-                            System.exit(0);
-                            break;
-                        }
-                        //right to left cross
-                        else if (a[0][2].equals(a[1][1]) && a[1][1].equals(a[2][0])) {
-                            System.out.println(player1);
-                            System.exit(0);
-                            break;
-                        }
-                    }
+
+                    player(a, "X");
+
+                    display(a);
+                    
+                    win(a, player1);
                 }
                 //player 2
                 else {
                     System.out.println("Choose the place in Player 2 -> O:");
                     search = s.nextInt();
-                    for (int i = 0; i < 3; i++) {
-                        for (int j = 0; j < 3; j++) {
-                            if ((a[i][j].equals(String.valueOf(search)))) {
-                                if (!a[i][j].equals("X") || !a[i][j].equals("O")) {
-                                    a[i][j] = "O";
-                                    player++;
-                                }
-                                else {
-                                    System.out.println("Please, Choose another place.");
-                                }
-                            }
-                        }
-                    }
-                    System.out.println(design);
-                    for (int i = 0; i < 3; i++) {
-                        System.out.print("|");
-                        for (int j = 0; j < 3; j++) {
-                            String n = String.valueOf(a[i][j]);
-                            System.out.print("\t" + n + "\t" + "|");
-                        }
-                        System.out.println("\n" + design);
-                    }
+
+                    player(a, "O");
+                    
+                    display(a);
+                    
+                    win(a, player2);
                 }
             }
             else{
                 System.out.println(draw);
                 System.exit(0);
             }
-            for(int i = 0; i<3; i++){
-                //hor
-                if(a[i][0].equals(a[i][1]) && a[i][1].equals(a[i][2]) && a[i][2].equals(a[i][0])){
-                    win(player2);
 
-                }
-                //ver
-                else if(a[0][i].equals(a[1][i]) && a[1][i].equals(a[2][i]) && a[2][i].equals(a[0][i])){
-                    win(player2);
-                }
-                //left to right cross
-                else if (a[0][0].equals(a[1][1]) && a[1][1].equals(a[2][2])) {
-                    win(player2);
-                    break;
-                }
-                //right to left cross
-                else if (a[0][2].equals(a[1][1]) && a[1][1].equals(a[2][0])) {
-                    win(player2);
-                    break;
+        }
+    }
+    void player(String[][] a, String option){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if ((a[i][j].equals(String.valueOf(search)))) {
+                    if (!a[i][j].equals("X") || !a[i][j].equals("O")) {
+                        a[i][j] = option;
+                        player++;
+                    }
+                    else {
+                        System.out.println("Please, Choose another place.");
+                    }
                 }
             }
         }
     }
-    void win(String player2)
+    void display(String[][] a){
+        System.out.println(design);
+        for (int i = 0; i < 3; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 3; j++) {
+                String n = String.valueOf(a[i][j]);
+                System.out.print("\t" + n + "\t" + "|");
+            }
+            System.out.println("\n" + design);
+        }
+    }
+    void win(String[][] a,String player2)
     {
-        System.out.println(player2);
-        System.exit(0);
+        for(int i = 0; i<3; i++){
+            //hor
+            if(a[i][0].equals(a[i][1]) && a[i][1].equals(a[i][2]) && a[i][2].equals(a[i][0])){
+                System.out.println(player2);
+                System.exit(0);
+
+            }
+            //ver
+            else if(a[0][i].equals(a[1][i]) && a[1][i].equals(a[2][i]) && a[2][i].equals(a[0][i])){
+                System.out.println(player2);
+                System.exit(0);
+            }
+            //left to right cross
+            else if (a[0][0].equals(a[1][1]) && a[1][1].equals(a[2][2])) {
+                System.out.println(player2);
+                System.exit(0);
+                break;
+            }
+            //right to left cross
+            else if (a[0][2].equals(a[1][1]) && a[1][1].equals(a[2][0])) {
+                System.out.println(player2);
+                System.exit(0);
+                break;
+            }
+        }
     }
 }
 public class Tictactoc extends Game{
